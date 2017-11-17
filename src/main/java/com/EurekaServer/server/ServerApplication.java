554794b98dc1bbafb18db56690e162ea75977ff9
ronnyling <ronnyckling@gmail.com>
2017-11-17 16:53:43 +0800

    initial

---
+++ src/main/java/com/EurekaServer/server/ServerApplication.java
@@ -0,0 +1,22 @@
+package com.EurekaServer.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableEurekaServer
+@EnableTurbine
+@EnableHystrixDashboard
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+}
+
